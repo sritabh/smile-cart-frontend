d8f27b5db52c6cfac6d0e3e51b636555dc6d9eb6
sritabh <sobydanny@gmail.com>
2024-06-17 15:52:40 +0530

    Implemented localization to smile cart

--- src/components/Cart/ProductCard.jsx
+++ src/components/Cart/ProductCard.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ProductQuantity from "components/commons/ProductQuantity";
 import { Delete } from "neetoicons";
 import { Typography, Alert } from "neetoui";
+import { useTranslation, Trans } from "react-i18next";
 import useCartItemsStore from "stores/useCartItemsStore";
 
 const ProductCard = ({
@@ -13,6 +14,7 @@ const ProductCard = ({
   name,
   availableQuantity,
 }) => {
+  const { t } = useTranslation();
   const [shouldShowDeleteAlert, setShouldShowDeleteAlert] = useState(false);
   const removeCartItem = useCartItemsStore.pickFrom();
 
@@ -24,8 +26,10 @@ const ProductCard = ({
           <Typography className="mb-2" style="h4" weight="bold">
             {name}
           </Typography>
-          <Typography style="body2">MRP: ${mrp}</Typography>
-          <Typography style="body2">Offer price: ${offerPrice}</Typography>
+          <Typography style="body2">{t("mrp", { mrp })}</Typography>
+          <Typography style="body2">
+            {t("offerPrice", { offerPrice })}
+          </Typography>
         </div>
         <div className="flex items-center space-x-2">
           <ProductQuantity {...{ availableQuantity, slug }} />
@@ -35,13 +39,13 @@ const ProductCard = ({
           />
           <Alert
             isOpen={shouldShowDeleteAlert}
-            submitButtonLabel="Yes, remove"
-            title="Remove item?"
+            submitButtonLabel={t("removeItemConfirmation.button")}
+            title={t("removeItemConfirmation.title")}
             message={
-              <Typography>
-                You are removing <strong>{name}</strong> from cart. Do you want
-                to continue?
-              </Typography>
+              <Trans
+                i18nKey="removeItemConfirmation.message"
+                values={{ name }}
+              />
             }
             onClose={() => setShouldShowDeleteAlert(false)}
             onSubmit={() => {
